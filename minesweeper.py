ac2cff6c3c4b6c39584e8742886683bb554a7d19
rAlphabet <rodic60@gmail.com>
2021-01-06 20:08:10 +0100

    Added mouseclick and markdown
    Player can now click on a cell with LMB (left mouse button) and with RMB (right mouse button). RMB will mark the cell as the potentional mine. LMB will 'take action' and reveal the cell (this functionality is yet to be added).

--- minesweeper.py
+++ minesweeper.py
@@ -230,3 +230,15 @@ while running:
             elif event.key == pygame.K_RETURN:
                 REVEALED = True
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            for cell in list_of_cells:
+                b = screen.blit(cell.img, (cell.x, cell.y))
+                if b.collidepoint(position):
+                    if event.button == 1:
+                        if not has_activated_timer:
+                            has_activated_timer = True
+                        # functionality missing
+                    elif event.button == 3:
+                        cell.marked = not cell.marked
+
