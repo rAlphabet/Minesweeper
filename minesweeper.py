562aed5adc29dfb6452ca9382fe29e7cb54f95cc
rAlphabet <rodic60@gmail.com>
2021-01-07 19:24:31 +0100

    Updated positions in instructions

--- minesweeper.py
+++ minesweeper.py
@@ -256,7 +256,7 @@ def instructions():
     pygame.draw.rect(screen, (125, 75, 255), (WIDTH - 200, 0, WIDTH, HEIGHT))
     stopwatch = font.render(f"TIME: {TIMER[1]}", True, black)
     screen.blit(stopwatch, (WIDTH - 175, 50))
-    screen.blit(settings_icon, (WIDTH - 164, 128))
+    screen.blit(settings_icon, (WIDTH - Settings.x_left, Settings.y))
     screen.blit(space_text, (WIDTH - 175, 226))
     screen.blit(restart_text, (WIDTH - 175, 258))
     screen.blit(rmb_text, (WIDTH - 175, 353))
