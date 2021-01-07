84f8666a94952cd426d911376a7a23e77a5597c4
rAlphabet <rodic60@gmail.com>
2021-01-07 19:23:40 +0100

    Added position to constructor of Settings

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ class Settings:
     """Class to represent settings for Minesweeper."""
     def __init__(self):
         self.size = 9
+        self.x_left = 164
+        self.y = 128
     
     def change_size(self, new_size):
         """Changes the size (of the grid)."""
