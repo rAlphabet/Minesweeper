2fd3f84f785d4fe8527588acdf5c4d3a367ba169
rAlphabet <rodic60@gmail.com>
2021-01-07 18:40:52 +0100

    Made size of the grid N x N
    It was made so that the grid is necessarily 'a square' (N x M is not possible, only N x N). This was made so that the images would not get distorted.

--- minesweeper.py
+++ minesweeper.py
@@ -5,8 +5,7 @@ from pygame import mixer
 #Constants, variables and path for the game.
 MINE = "X"
 FILES = r"C:\Users\Mojca\Desktop\gitProjects\Minesweeper\files"
-MS_WIDTH = 9
-MS_HEIGHT = 9
+MS_WIDTH = MS_HEIGHT = 9
 OBJ_WIDTH = round(64 * (1 / (MS_WIDTH/9)))
 OBJ_HEIGHT = round(64 * (1 / (MS_HEIGHT/9)))
 SIZE = WIDTH, HEIGHT = MS_WIDTH * (OBJ_WIDTH + 2) + 200, MS_HEIGHT * (OBJ_HEIGHT + 2) - 2
