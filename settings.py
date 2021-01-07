a5111f9df1a18570c16811f0b1f084b956519f6d
rAlphabet <rodic60@gmail.com>
2021-01-07 18:48:44 +0100

    Added method change_size to class Settings
    This method changes the size of the grid.

--- settings.py
+++ settings.py
@@ -3,4 +3,8 @@ class Settings:
     def __init__(self):
         self.size = 9
     
+    def change_size(self, new_size):
+        """Changes the size (of the grid)."""
+        self.size = new_size
+    
     
