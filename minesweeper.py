8987ddce5cbb9ff779c17f9d6e5358deb10e26d7
rAlphabet <rodic60@gmail.com>
2021-01-08 21:16:15 +0100

    Fixed path for files
    Imported os. Fixed path so that the script can run on all computers.

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,4 @@
+import os
 import random
 import pygame
 from pygame import mixer
@@ -61,7 +62,8 @@ class Settings:
 #Constants, variables and path for the game.
 Settings = Settings()
 MINE = "X"
-FILES = r"C:\Users\Mojca\Desktop\gitProjects\Minesweeper\files"
+directory_path = os.getcwd()
+FILES = directory_path + r"/files"
 MS_WIDTH = MS_HEIGHT = Settings.size
 OBJ_WIDTH = OBJ_HEIGHT = round(64 * (1 / (MS_WIDTH / 9)))
 SIZE = WIDTH, HEIGHT = MS_WIDTH * (OBJ_WIDTH + 2) + 200, MS_HEIGHT * (OBJ_HEIGHT + 2) - 2
