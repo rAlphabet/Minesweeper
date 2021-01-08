39b60dbb037354a091d73a5ee763465e208038f6
rAlphabet <rodic60@gmail.com>
2021-01-08 18:14:55 +0100

    Fixed OBJ_WIDTH and OBJ_HEIGHT
    Previous commit was a mistake. In OBJ_WIDTH and OBJ_HEIGHT, MS_WIDTH must be divided by 9 !

--- minesweeper.py
+++ minesweeper.py
@@ -52,7 +52,7 @@ Settings = Settings()
 MINE = "X"
 FILES = r"C:\Users\Mojca\Desktop\gitProjects\Minesweeper\files"
 MS_WIDTH = MS_HEIGHT = Settings.size
-OBJ_WIDTH = OBJ_HEIGHT = round(64 * (1 / (MS_WIDTH / Settings.size)))
+OBJ_WIDTH = OBJ_HEIGHT = round(64 * (1 / (MS_WIDTH / 9)))
 SIZE = WIDTH, HEIGHT = MS_WIDTH * (OBJ_WIDTH + 2) + 200, MS_HEIGHT * (OBJ_HEIGHT + 2) - 2
 CAPTION = "Minesweeper"
 FPS = 24
